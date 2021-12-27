4cd08242a70c524a772758c0b6d2e2431913ae49
“Ahmad <ahmadabotoimah@gmail,com>
2021-12-27 15:47:02 +0200

    error in styling admin

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
+//use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CommentController;
 
-use App\Http\Controllers\AdminController;
+//use App\Http\Controllers\AdminController;
 
 
 use App\Http\Controllers\TrendingController;
 //use App\Http\Controllers\PostController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CountryController;
+//use App\Http\Controllers\CountryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,7 +29,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('aut
 Route::get('/search/', 'App\Http\Controllers\HomeController@search')->name('search')->middleware('auth');
 
 /*countries*/
-Route::get('/signup', [CountryController::class, 'index'])->middleware('auth')->name('register');
+//Route::get('/signup', [CountryController::class, 'index'])->middleware('auth')->name('register');
 
 
 Route::get('/chat', function () {
@@ -51,12 +52,12 @@ Route::get('/trending', [TrendingController::class, 'index'])->name('trending');
 Route::resource('comments', CommentController::class);
 
 /*countries*/
-Route::get('/reg', [CountryController::class, 'index']);
+Route::resource('/signup', CountryController::class, )->name('register','index');
 
 Route::post('/like-post/{id}',[App\Http\Controllers\PostController::class,'likePost'])->name('like.post');
 Route::post('/unlike-post/{id}',[App\Http\Controllers\PostController::class,'unlikePost'])->name('unlike.post');
 
 // Admin
 Route::resource('/admoon', AdminController::class);
-Route::get('/admooon', [AdminController::class, 'index2'])->name('admooon');
-Route::get('/manageadmoon', [AdminController::class, 'index3'])->name('admoonprofile');
+Route::get('/admooon', [App\Http\Controllers\AdminController::class, 'index2'])->name('admooon');
+Route::get('/manageadmoon', [App\Http\Controllers\AdminController::class, 'index3'])->name('admoonprofile');
