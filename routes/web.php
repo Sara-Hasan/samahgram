03053049fdc83c2913f237338a563e7448734729
“Ahmad <ahmadabotoimah@gmail,com>
2021-12-27 14:51:33 +0200

    ddddd

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\FollowController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CommentController;
@@ -8,7 +10,6 @@ use App\Http\Controllers\AdminController;
 
 
 use App\Http\Controllers\TrendingController;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CountryController;
 /*
@@ -18,7 +19,7 @@ use App\Http\Controllers\CountryController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return view('home');
 })->middleware('auth');
 
@@ -45,7 +46,6 @@ Route::get('/reg', [CountryController::class, 'index'])->middleware('auth')->nam
 
 Route::get('/chat', function () {
    return view('chat');
-    return view('chat');
 })->name('chat');
 
 Route::get('/AboutUs', function () {
@@ -62,14 +62,14 @@ Route::get('/trending', function () {
 //     return view('setting');
 // })->name('setting');
 
-Route::get('/profile/{id}', [App\Http\Controllers\HomeController::class, 'show'])->name('profile');
-Route::put('/profile/{id}', [App\Http\Controllers\HomeController::class, 'update'])->name('profile.update');
-Route::get('/profile/setting/{id}', [App\Http\Controllers\HomeController::class, 'setting'])->name('setting');
-Route::resource('/posts', 'App\Http\Controllers\PostController');
+Route::get('/profile/{id}', [HomeController::class, 'show'])->name('profile');
+Route::put('/profile/{id}', [HomeController::class, 'update'])->name('profile.update');
+Route::get('/profile/setting/{id}', [HomeController::class, 'setting'])->name('setting');
+Route::resource('/posts', PostController::class);
 // Route::resource('/follows', 'App\Http\Controllers\FollowController');
 // Route::post('/follows/{id}', [App\Http\Controllers\FollowController::class, 'store'])->name('follow');
 // Route::post('/follows/delete/{id}', [App\Http\Controllers\FollowController::class, 'destroy'])->name('UnFollow');
-Route::resource('/follows', 'App\Http\Controllers\FollowController');
+Route::resource('/follows', FollowController::class);
 
 Route::get('/trending', [TrendingController::class, 'index'])->name('trending');
 
@@ -83,9 +83,9 @@ Route::post('/unlike-post/{id}',[PostController::class,'unlikePost'])->name('unl
 Route::get('/post/{id}', [PostController::class, 'show'])->name('post');
 
 
-// Admin 
+// Admin
 
-	
+
 
 
 //Route::get('/admoon', function () {
